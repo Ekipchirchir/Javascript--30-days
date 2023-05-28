e1a6e00eb2df28baea93938a3e043ac8e7807f99
Ekipchirchir <130069395+Ekipchirchir@users.noreply.github.com>
2023-05-28 10:42:47 +0300

    Add files via upload

---
+++ Day 27 Portfolio/portfolio.js
@@ -0,0 +1,5 @@
+const emailElement = document.getElementById("email");
+emailElement.textContent = "manuuchirchir50@gmail.com";
+
+const phoneElement = document.getElementById("phone");
+phoneElement.textContent = "+2547-696-2512";

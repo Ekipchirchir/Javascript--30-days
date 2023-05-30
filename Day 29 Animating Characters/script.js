e83380d3b7b3d61f35decee105fa125f8ca0a423
Ekipchirchir <130069395+Ekipchirchir@users.noreply.github.com>
2023-05-30 11:04:32 +0300

    Add files via upload

---
+++ Day 29 Animating Characters/script.js
@@ -0,0 +1,18 @@
+// Wrap every letter in a span
+const textWrapper = document.querySelector('.container');
+textWrapper.innerHTML = textWrapper.textContent.replace();
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.container .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 2250,
+    delay: (el, i) => 150 * (i+1)
+  }).add({
+    targets: '.container',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
